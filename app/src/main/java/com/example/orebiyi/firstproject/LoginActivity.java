c308860c868795c1006ab748296e5dc8e058653d
Khuzaima Tariq <tariqmahmood@yandex.com>
2025-06-17 23:37:58 +0500

    lec 6

--- app/src/main/java/com/example/orebiyi/firstproject/LoginActivity.java
+++ app/src/main/java/com/example/orebiyi/firstproject/LoginActivity.java
@@ -7,26 +7,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import es.dmoral.toasty.Toasty;
 
 public class LoginActivity extends AppCompatActivity {
 
-    String defaultEmail = "tariq.mahmood@au.edu.pk";
-    String defaultPassword = "12345678";
+    FirebaseAuth auth;
 
     TextInputEditText email;
     TextInputEditText password;
     MaterialButton login;
     TextView signup;
+
     @Override
     //oncreate method. the first method which is invoked/called when application starts
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,9 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        auth = FirebaseAuth.getInstance();
+
         email = findViewById(R.id.edit_email);
         password = findViewById(R.id.edit_password);
         login = findViewById(R.id.login_button);
@@ -50,37 +58,43 @@ public class LoginActivity extends AppCompatActivity {
                 String etPassword = password.getText().toString().trim();
 
                 //check if email and password are empty?
-                if(etEmail.isEmpty()){
-                  // email.setError("Email is required");
-                   Toasty.error(LoginActivity.this, "Email is required", Toast.LENGTH_SHORT).show();
+                if (etEmail.isEmpty()) {
+                    // email.setError("Email is required");
+                    Toasty.error(LoginActivity.this, "Email is required", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(etPassword.isEmpty()){
+                if (etPassword.isEmpty()) {
                     //password.setError("Password is required");
                     Toasty.error(LoginActivity.this, "Password is required", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 //check email pattern
-                if(!android.util.Patterns.EMAIL_ADDRESS.matcher(etEmail).matches()){
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(etEmail).matches()) {
                     //email.setError("Invalid email pattern");
                     Toasty.error(LoginActivity.this, "Invalid email pattern", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                //TODO YOUR TASK FROM HERE
-                //check length of password - minimum 8 characters
-                //check user entered email is matched with default email or not
-                //check user entered password in matched with default password or not
+                auth.signInWithEmailAndPassword(etEmail, etPassword)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                                    startActivity(i);
+                                } else {
+                                    Toasty.error(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
 
-                Intent i= new Intent(LoginActivity.this,HomeActivity.class);
-                startActivity(i);
             }
         });
         signup = findViewById(R.id.signup_text);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= new Intent(LoginActivity.this,SignupActivity.class);
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(i);
             }
         });
