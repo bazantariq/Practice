90b5fd16a09a6d8f45905fb2321f73e8e991f059
Khuzaima Tariq <tariqmahmood@yandex.com>
2025-06-17 23:38:42 +0500

    lec 6

---
+++ app/src/main/java/com/example/orebiyi/firstproject/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.orebiyi.firstproject;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import es.dmoral.toasty.Toasty;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+        Toasty.success(HomeActivity.this, "Welcome to Home Activity", Toasty.LENGTH_SHORT).show();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
